fd14c3f62daf8dc3f39ab5dcad114dc309f7279e
hamid2baika <131192112+hamid2baika@users.noreply.github.com>
2023-04-18 20:36:21 +0200

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTracks } from './lib/fetchTracks';
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -9,11 +11,18 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
+
 const App = () => {
   const [trackIndex, setTrackIndex] = useState(0);
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
   };
+  const { data: tracks } = useQuery({
+		queryKey: ['tracks'],
+		queryFn: fetchTracks
+  });
+
+  
   return (
     <div className="App">
       <header className="App-header">
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,18 @@
-const apiToken: string = '';
+
+const apiToken = 'BQDbsQDoObAUzsep30F1VnmGvcKx4E6v8dGzsNSRkvRyHUuR4gZ4swquwBT7ivsF73AYdtZMII-wtrcGhiXdnlUEWICG2s01MF--NKXscO1V7j-A0fn-eWNyb6jOXDRgRRbX48iuHZ7R3RKG2hyOGCWiW6f9-ZjTVQ6apkBV4bKba3G359aSZOK9gU7g3WyZSGNzRfftMncdnvWdc0j7tVOlVKkI3v208pPh_uyV2QnrhQHaxPQ0flL6KZ2us65vYU4XtIdZ2jtICCaAL91K-TuwTfBycgQAO2Jbyol5aTQlBRx8rbKarbiWljycwKJ0BNHqITalz1lMLtWeU7fq0ppXocVfz2GEeoBJJLnEmWW-Lpg
+';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+     throw new Error(`Fetching tracks failed with status ${response.status}`)
+   }
+  const data = await response.json() as { items: unknown[] };
+
+  return data.items;
 };
